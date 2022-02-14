17be51c9dd509e26c999f085ca17fb5218124f95
Abiud Salazar <abiud.salazar@alumno.buap.mx>
2022-02-14 14:26:11 -0600

    ejemplo de carrito terminado

--- src/App.js
+++ src/App.js
@@ -8,19 +8,60 @@ class App extends Component {
   state = {
     productos: [
       { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
-      { name: 'Chicharos', price: 2500, img: '/productos/arbejas.jpg' },
+      { name: 'Chícharos', price: 2500, img: '/productos/arbejas.jpg' },
       { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' },
-    ]
+    ],
+    carro: [
+      //{ name: 'Tomate', price: 1500, img: '/productos/tomate.jpg', cantidad: 0 },
+      //{ name: 'Chicharos', price: 2500, img: '/productos/arbejas.jpg', cantidad: 0 },
+      //{ name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg', cantidad: 0 },
+    ],
+    esCarroVisible: false,
+  }
+
+  agregarAlCarro = ( producto ) => {
+    const { carro } = this.state;
+
+    if( carro.find( x => x.name === producto.name) ){
+      const newCarro = carro.map( x => x.name === producto.name 
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1 
+        })
+        : x
+      )
+      return this.setState({ carro: newCarro })
+    }
+
+    //console.log(producto);
+    return this.setState({
+      carro: this.state.carro.concat({ 
+        ...producto,
+        cantidad: 1 
+      })
+    })
+  }
+
+  mostrarCarro = () => {
+    if( !this.state.carro.length ){
+      return;
+    }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible })
   }
 
   render(){
+    const { esCarroVisible } = this.state;   
     return(
       <div>
-        <Navbar />
+        <Navbar 
+          carro={this.state.carro} 
+          esCarroVisible={esCarroVisible}
+          mostrarCarro={this.mostrarCarro}
+        />
         <Layout>
           <Title />
           <Productos 
-            agregarAlCarro={() => console.log('No hace nada')}
+            agregarAlCarro={ this.agregarAlCarro }
             productos={this.state.productos}
           />        
         </Layout>
